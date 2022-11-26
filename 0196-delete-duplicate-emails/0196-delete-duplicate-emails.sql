cd747fbc12dc4707a103a6a31e8661c814fcf53e
Jatin Garg <97427760+JatinGarg19@users.noreply.github.com>
2022-11-26 22:08:06 +0530

    Time: 1019 ms (67.19%), Space: 0B (100.00%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -4,3 +4,13 @@
 delete p1
 from Person as p1, Person as p2
 where p1.email = p2.email and p1.id > p2.id;
+
+
+# delete p.*
+# from Person as p, (
+#     select email, min(id) as minId
+#     from Person
+#     group by email
+#     having count(*) > 1
+# ) as q
+# where p.email = q.email and id > q.minId;
