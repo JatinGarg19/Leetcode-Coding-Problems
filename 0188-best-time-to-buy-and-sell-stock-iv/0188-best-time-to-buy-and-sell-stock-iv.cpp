41dc18b4cbfe639e1e08231fd7a07c2df0f8b5f8
Jatin Garg <97427760+JatinGarg19@users.noreply.github.com>
2022-12-28 15:05:01 +0530

    Time: 26 ms (55.25%), Space: 14.4 MB (31.15%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    // Memoization Solution
+    int f(int ind, int buy, int cap, vector<int> &prices, vector<vector<vector<int>>> &dp) {
+        int n = prices.size();
+        
+        if(cap == 0) return 0;
+        if(ind == n) return 0;
+        
+        int profit = 0;
+        
+        if(dp[ind][buy][cap] != -1) return dp[ind][buy][cap];
+        
+        // Can only Buy
+        if(buy) {
+            profit = max(-prices[ind] + f(ind + 1, 0, cap, prices, dp), 
+                             0 + f(ind + 1, 1, cap, prices, dp));
+        }
+        // You can only sell
+        else {
+            profit = max(prices[ind] + f(ind + 1, 1, cap - 1, prices, dp),
+                             0 + f(ind + 1, 0, cap, prices, dp));
+        }
+        
+        return dp[ind][buy][cap] = profit;
+    }
+    
+    // Tabulation Solution
+    int maxProfit(int k, vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(k + 1, 0)));
+        
+        for(int ind = n - 1; ind >= 0; ind--) {
+            for(int buy = 0; buy <= 1; buy++) {
+                for(int cap = 1; cap <= k; cap++) {
+                    int profit = 0;
+                    
+                    // Can only Buy
+                    if(buy) {
+                        profit = max(-prices[ind] + dp[ind + 1][0][cap], 
+                                        0 + dp[ind + 1][1][cap]);
+                    }
+                    // You can only sell
+                    else {
+                        profit = max(prices[ind] + dp[ind + 1][1][cap - 1],
+                                        0 + dp[ind + 1][0][cap]);
+                    }
+        
+                    dp[ind][buy][cap] = profit;
+                }
+            } 
+         }
+        
+        return dp[0][1][k];
+    }
+};
