e7340994c408fd5e94a5219d7c3857e75b9eb25f
Jatin Garg <97427760+JatinGarg19@users.noreply.github.com>
2023-01-28 11:02:29 +0530

    Time: 180 ms (12.94%), Space: 33.5 MB (38.43%) - LeetHub

---
+++ 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.cpp
@@ -0,0 +1,52 @@
+class SummaryRanges {
+    // set to store the addNumbers.
+    set<int> values;
+    
+public:
+    SummaryRanges() { }
+    
+    void addNum(int value) {
+        // whatever value is there, push onto the set
+        values.insert(value);
+    }
+    
+    vector<vector<int>> getIntervals() {
+        // If our container is empty, we return empty vector.
+        if(values.empty())
+            return {};
+        
+        // answer array
+        vector<vector<int>> intervals;
+        
+        // Starting interval initialization
+        int left = -1, right = -1;
+        
+        for(int value : values) {
+            // Update left and right with the 1st value that comes
+            if(left < 0) {
+                left = right = value;
+            }
+            
+            // If right + 1 becomes = value, update right
+            else if(value == right + 1) {
+                right = value;
+            }
+            
+            // else push interval into ans array and update left and right
+            else {
+                intervals.push_back({left, right});
+                left = right = value;
+            }
+        }
+        
+        intervals.push_back({left, right});
+        return intervals;
+    }
+};
+
+/**
+ * Your SummaryRanges object will be instantiated and called as such:
+ * SummaryRanges* obj = new SummaryRanges();
+ * obj->addNum(value);
+ * vector<vector<int>> param_2 = obj->getIntervals();
+ */
