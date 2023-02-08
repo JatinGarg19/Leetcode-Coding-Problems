a77a06bb72522802cb46a546f17ea3a13bc9d358
Jatin Garg <97427760+JatinGarg19@users.noreply.github.com>
2023-02-08 23:34:52 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (78.73%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -1,46 +1,31 @@
-class Solution {
+class Solution {   
 public:
     string decodeString(string s) {
-        int n = s.size();
+        int pos = 0;
+        return solve(s , pos);
+    }
+    
+    string solve(string s, int &pos) {
+        int num = 0;
         string ans = "";
         
-        stack<char> st;
-        
-        for(int i = 0; i < n; i++) {
-            if(s[i] == ']') {
-                string temp;
+        for( ; pos < s.size(); pos++) {
+            char curr = s[pos];
+            
+            if(curr == '[') {
+                string currStr = solve(s , ++pos);
                 
-                
-                while(st.top() != '[') {
-                    temp = st.top() + temp;
-                    st.pop();
-                }
-                
-                st.pop();
-                
-                string number = "";
-                
-                while(!st.empty() && isdigit(st.top())) {
-                    number = st.top() + number;
-                    st.pop();
-                }
-                
-                int k = stoi(number);
-                
-                while(k--) {
-                    for(int i = 0; i < temp.size(); i++) {
-                        st.push(temp[i]);
-                    }
-                }
+                for( ; num > 0; num--)
+                    ans += currStr;
             }
-            else {
-                st.push(s[i]);
-            }   
-        }
-        
-        while(st.size() != 0) {
-            ans = st.top() + ans;
-            st.pop();
+            
+            else if(curr >= '0' && curr <= '9') {
+                num = (num * 10) + curr - '0';
+            }
+            else if(curr == ']')
+                return ans;
+            else
+                ans += curr;
         }
         
         return ans;
