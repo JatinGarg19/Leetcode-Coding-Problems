008658e9860a0cf0a512aac7956028b470e4e8eb
Jatin Garg <97427760+JatinGarg19@users.noreply.github.com>
2023-02-14 14:04:51 +0530

    Time: 5 ms (42.01%), Space: 6.4 MB (61.65%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,57 +1,28 @@
 class Solution {
 public:
-    string solve(string a , string b) {
-        if(a.size() > b.size())
-            return solve(b , a);
-        
-        int diff = b.size() - a.size();
-        
-        string padding;
-        for(int i = 0; i < diff; i++) {
-            padding.push_back('0');
-        }
-        
-        a = padding + a;
-        
-        string res;
-        char carry = '0';
-        
-        for(int i = a.size() - 1; i >= 0; i--) {
-            if(a[i] == '1' && b[i] == '1') {
-                if(carry == '1')
-                    res.push_back('1') , carry = '1';
-                else 
-                    res.push_back('0') , carry = '1';
-            }
-            
-            else if(a[i] == '0' && b[i] == '0') {
-                if(carry == '1')
-                    res.push_back('1') , carry = '0';
-                else 
-                    res.push_back('0') , carry = '0';
-            }
-            
-            else if(a[i] != b[i]) {
-                if(carry == '1')
-                    res.push_back('0') , carry = '1';
-                else 
-                    res.push_back('1') , carry = '0';
-            }
-        }
-        
-        if(carry == '1')
-            res.push_back(carry);
-        
-        reverse(res.begin() , res.end());
-        
-        int idx = 0;
-        while(idx + 1 < res.size() && res[idx] == '0')
-            idx++;
-        
-        return res.substr(idx);
-    }
-    
     string addBinary(string a, string b) {
-        return solve(a , b);
+        int i = a.size() - 1;
+        int j = b.size() - 1;
+        string ans;
+        int carry = 0;
+        
+        while(i >= 0 || j >= 0 || carry) {
+            if(i >= 0) {
+                carry += a[i] - '0';
+                i--;
+            }
+            
+            if(j >= 0) {
+                carry += b[j] - '0';
+                j--;
+            }
+            
+            ans += (carry % 2 + '0');
+            carry = carry / 2;
+        }
+        
+        reverse(ans.begin() , ans.end());
+        
+        return ans;
     }
 };
