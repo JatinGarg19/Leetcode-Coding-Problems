c63feb2a4fbaba871335ba7139436f82e56efa31
Jatin Garg <97427760+JatinGarg19@users.noreply.github.com>
2023-02-15 12:12:14 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (43.57%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.cpp
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
+        // Maintain count of stones that are jewels. 
         int count = 0;
         
+        // HashTable - char , idx pair.
         unordered_map<char,int> map;
         
+        // Push characters of jewels into the map.
         for(int i = 0; i < jewels.size(); i++) {
             map[jewels[i]] = i;
         }
         
         for(int j = 0; j < stones.size(); j++) {
+            // If the corresponding stone is a jewel, increment the counter.
             if(map.find(stones[j]) != map.end()) {
                 count++;
             }
         }
         
+        // return answer.
         return count;
     }
 };
