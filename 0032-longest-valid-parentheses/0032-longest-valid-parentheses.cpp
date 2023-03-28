cd5fc94d4199736669f2d0b5df2fdf4b9b79b355
Jatin Garg <97427760+JatinGarg19@users.noreply.github.com>
2023-03-28 14:15:29 +0530

    Time: 7 ms (36.62%), Space: 6.9 MB (85.87%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -2,23 +2,47 @@ class Solution {
 public:
     int longestValidParentheses(string s) {
         int n = s.size();
-        int maxLength = 0;
+        int maxL = 0;
+        // left for '(' and right for ')' count
+        int left = 0, right = 0;
         
-        stack<int> stk; stk.push(-1);
-        
+        // Scan from left to right
         for(int i = 0; i < n; i++) {
+            // opening bracket - increment left
             if(s[i] == '(')
-                stk.push(i);
-            else {
-                stk.pop();
-                if(stk.empty()) {
-                    stk.push(i);
-                } else {
-                    maxLength = max(maxLength , i - stk.top());
-                }
-            }
+                left++;
+            // closing bracket - increment right
+            else 
+                right++;
+            
+            // reset l and r if r > l.
+            if(right > left)
+                left = right = 0;
+            // if equal, update maxL.
+            else if(left == right)
+                maxL = max(maxL, 2*right);
         }
         
-        return maxLength;   // Tc - O(n) , Sc - O(n).
+        // reset both l and r to 0.
+        left = right = 0;
+        
+        // Scan from right to left - to handle cases if count of '(' is more than ')'.
+        for(int i = n - 1; i >= 0; i--) {
+            // same as above.
+            if(s[i] == '(')
+                left++;
+            else 
+                right++;
+            
+            // if l > r, reset to 0.
+            if(left > right)
+                left = right = 0;
+            // if equal , update maxL.
+            else if(left == right)
+                maxL = max(maxL, 2*left);
+        }
+        
+        // return the final maxL
+        return maxL;
     }
 };
