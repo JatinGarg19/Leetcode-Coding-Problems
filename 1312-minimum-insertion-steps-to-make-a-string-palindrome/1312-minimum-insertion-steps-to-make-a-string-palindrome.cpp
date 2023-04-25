fa092654e05e651d89fb3fb9ce93751bdbad7717
Jatin Garg <97427760+JatinGarg19@users.noreply.github.com>
2023-04-25 22:55:54 +0530

    Time: 119 ms (10.98%), Space: 28.6 MB (19.74%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minInsertions(string s) {
+        int n = s.size();
+        int x = longestPalindromeSubseq(s);
+        return n - x;
+    }
+    
+    int longestPalindromeSubseq(string &s) {
+        string t = s;
+        reverse(t.begin(), t.end());
+        return lcs(s, t);
+    }
+    
+    int lcs(string &s, string &t) {
+        int n = s.size();
+        int m = t.size();
+        
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        
+        return f(n-1, m-1, s, t, dp);
+    }
+    
+    int f(int idx1, int idx2, string &s, string &t, vector<vector<int>>& dp) {
+        if(idx1 < 0 || idx2 < 0) return 0;
+        
+        if(dp[idx1][idx2] != -1) return dp[idx1][idx2];
+        
+        if(s[idx1] == t[idx2]) {
+            return dp[idx1][idx2] = 1 + f(idx1-1, idx2-1, s, t, dp);
+        }
+        else {
+           return dp[idx1][idx2] = 0 + max(f(idx1, idx2-1, s, t, dp), f(idx1-1, idx2, s, t, dp)); 
+        }
+    }
+};
